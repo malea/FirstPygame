3dc17556417641c6c1efc2c81d163e8ee8f34e28
Malea Grubb <maleangrubb@gmail.com>
2014-02-09 00:13:01 -0500

    Make main

--- sillygame.py
+++ sillygame.py
@@ -113,26 +113,62 @@ class Clown(pygame.sprite.Sprite):
         if not self.dizzy:
             self.dizzy = 1
             self.original = self.image
-   
-#initialize stuff         
-pygame.init()
-screen = pygame.display.set_mode((468,60))
-pygame.display.set_caption('Clowning Around')
-pygame.mouse.set_visible(0)
 
-#create background
-background = pygame.Surface(screen.get_size())
-background = background.convert()
-background.fill((250,250,250))
+def main():
+  
+    #initialize stuff         
+    pygame.init()
+    screen = pygame.display.set_mode((468,60))
+    pygame.display.set_caption('Clowning Around')
+    pygame.mouse.set_visible(0)
 
-if pygame.font:
-    font = pygame.font.Font(None, 36)
-    text = font.render("Punch the Clown...Clowns are EVIL!", 1, (10,10,10))
-    textpos = text.get_rect(centerx=background.get_width()/2)
-    background.blit(text, textpos)
+    #create background
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((250,250,250))
+
+    if pygame.font:
+        font = pygame.font.Font(None, 36)
+        text = font.render("Punch the Clown...Clowns are EVIL!", 1, (10,10,10))
+        textpos = text.get_rect(centerx=background.get_width()/2)
+        background.blit(text, textpos)
     
+    #create objects
+    whiff_sound = load_sound('whiff.wav')
+    punch_sound = load_sound('punch.wav')
+    clown = Clown()
+    fist = Fist()
+    allsprites = pygame.sprite.RenderPlain((fist, clown))
+    clock = pygame.time.Clock()
 
+    #game runs on infinite loop
+    while 1:
+        clock.tick(60)
 
+        #simple working event queue
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return
+            elif event.type == KEYDOWN and event.key == K_ESCAPE:
+                return
+            elif event.type == MOUSEBUTTONDOWN:
+                if fist.punch(chimp):
+                    punch_sound.play() #punch
+                    chimp.punched()
+                else:
+                    whiff_sound.play() #miss
+            elif event.type == MOUSEBUTTONUP:
+                fist.unpunch()
 
+        #update sprites
+        allsprites.update()
+
+        #draw scene
+        screen.blit(background, (0, 0))
+        allsprites.draw(screen)
+        pygame.display.flip()
+        
+#Game Over
     
-
+#this calls the 'main' function when this script is executed
+if __name__ == '__main__': main()
