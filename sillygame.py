3d675e7c05b16e0c201714a8048402efc51c03b6
Malea Grubb <maleangrubb@gmail.com>
2014-02-08 23:54:56 -0500

    Add sprites, set up initialization and background

--- sillygame.py
+++ sillygame.py
@@ -43,6 +43,89 @@ def load_sound(name):
         raise SystemExit, message
     return sound
     
-    
+class Fist(pygame.sprite.Sprite):
+    #moves a clenched fist on the screen, following the mouse
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self) #call Sprite initializer
+        self.image, self.rect = load_image('fist.bmp', -1)
+        self.punching = 0
+
+    def update(self):
+        #move the fist based on the mouse position
+        pos = pygame.mouse.get_pos()
+        self.rect.midtop = pos
+        if self.punching:
+            self.rect.move_ip(5, 10)
+
+    def punch(self, target):
+        #returns true if the fist collides with the target"
+        if not self.punching:
+            self.punching = 1
+            hitbox = self.rect.inflate(-5, -5)
+            return hitbox.colliderect(target.rect)
+
+    def unpunch(self):
+        #called to pull the fist back"
+        self.punching = 0
+
+class Clown(pygame.sprite.Sprite):
+    #move clown around screen
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self) #call Sprite intializer
+        self.image, self.rect = load_image('clown.bmp', -1)
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.rect.topleft = 10, 10
+        self.move = 9
+        self.dizzy = 0
+
+    def update(self):
+        if self.dizzy: #if punched
+            self._spin()
+        else: #if not punched
+            self._walk()
+
+    def _walk(self):
+        #move clown around and turn if at edge
+        newpos = self.rect.move((self.move, 0))
+        if not self.area.contains(newpos):
+	if self.rect.left < self.area.left or \
+		self.rect.right > self.area.right:
+            self.move = -self.move
+            newpos = self.rect.move((self.move, 0))
+            self.image = pygame.transform.flip(self.image, 1, 0)
+        self.rect = newpos
+
+    def _spin(self):
+        #spin the clown
+        center = self.rect.center
+        self.dizzy += 12
+        if self.dizzy >= 360:
+            self.dizzy = 0
+            self.image = self.original
+        else:
+            rotate = pygame.transform.rotate
+            self.image = rotate(self.original, self.dizzy)
+        self.rect = self.image.get_rect(center=center)
+
+    def punched(self):
+        #this makes the clow spin
+        if not self.dizzy:
+            self.dizzy = 1
+            self.original = self.image
+   
+#initialize stuff         
+pygame.init()
+screen = pygame.display.set_mode((468,60))
+pygame.display.set_caption('Clowning Around')
+pygame.mouse.set_visible(0)
+
+#create background
+background = pygame.Surface(screen.get_size())
+background = background.convert()
+background.fill((250,250,250))
+
+
+
     
 
